5c870900ac1ef774a7a9b2e531a0823cf6297f69
晴央 山口 <e1c19100@oit.ac.jp>
2021-10-07 15:25:42 +0900

    一週目

--- WinOGL/WinOGLView.h
+++ WinOGL/WinOGLView.h
@@ -3,6 +3,7 @@
 //
 
 #pragma once
+#include <gl/GL.h>
 
 
 class CWinOGLView : public CView
@@ -14,6 +15,7 @@ protected: // シリアル化からのみ作成します。
 // 属性
 public:
 	CWinOGLDoc* GetDocument() const;
+	double ClickX, ClickY;
 
 // 操作
 public:
@@ -39,6 +41,13 @@ protected:
 	DECLARE_MESSAGE_MAP()
 public:
 	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+private:
+	HGLRC m_hRC;
+public:
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+	afx_msg void OnDestroy();
+	afx_msg BOOL OnEraseBkgnd(CDC* pDC);
+	afx_msg void OnSize(UINT nType, int cx, int cy);
 };
 
 #ifndef _DEBUG  // WinOGLView.cpp のデバッグ バージョン
